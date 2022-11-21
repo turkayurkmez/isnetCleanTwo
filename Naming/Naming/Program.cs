5ed9a7049168af8cdc4c64ad6d44beb7c4b0ecef
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-21 13:42:23 +0300

    talking about naming

--- Naming/Naming/Program.cs
+++ Naming/Naming/Program.cs
@@ -1,2 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
 Console.WriteLine("Hello, World!");
+
+// anlamlı isimler bulmak.
+int x; // kullanıcının kaydından bu yana geçen süre....
+int daysSinceUserCreation = 15;
+int musteriKaydindanBuYanaGecenGunSayisi = 10; //Evrensel dil elbette ingilizce olmalı....
+
+var hp = 1;
+//1. Horse power
+//2. Health Point
+//3. HP laptop sayısı
+//4. Hit Point...
+
+var healthPoint = 100;
+DataTable table = new DataTable();
+
+List<string> emailAdresses = new List<string>();
+List<string> emailList = new List<string>();
+
+int intNumber = 0;
+
+char charLetter = 'a';
+
+var o = 0;
+var l = 1;
+
+
+if (o == l)
+{
+
+}
+
+List<int[]> cells = new List<int[]>();
+
+List<int[]> getFlaggedCellsOnGameBoard()
+{
+    List<int[]> flaggedCells = new List<int[]>();
+    foreach (var cell in cells)
+    {
+        if (cell[0] == Flags.Flagged) //4 nedir? 
+        {
+            flaggedCells.Add(cell);
+        }
+    }
+    return flaggedCells;
+}
+
+
+
+enum FlagState
+{
+    Empty = 0,
+    Flagged = 4
+};
+
+public class Flags
+{
+    public const int Empty = 0;
+    public const int Flagged = 4;
+}
---
+++ Naming/Naming/WordPuzzle.cs
@@ -0,0 +1,41 @@
+﻿namespace Naming
+{
+    public class WordPuzzle
+    {
+        /*
+         * 1. Belirli bir dosyadan rastgele bir kelime seç.
+         * 2. Bu kelimenin her harfini *'a dönüştür.
+         * 3. Kullanıcının girdiği harfi denetle.
+         * 4. Harf varsa; ilgili harfin bulunduğu pozisyondaki *'ı harf ile değiştir.
+         * 5. Tahmin etmesini iste.
+         * 6. Tekrar et...
+         * 
+         */
+
+        public string GetRandomWordInFile(string wordsFilePath)
+        {
+            return string.Empty;
+        }
+
+        public string GetPuzzledWord(string realWord)
+        {
+            return string.Empty;
+        }
+
+        public bool IsWordContain(char letter)
+        {
+            return false;
+        }
+
+        public string GetPuzzledWordWithLetterIsFind(string realWord, char letter)
+        {
+            return string.Empty;
+        }
+
+        public void TakeGuest(string guestingWord)
+        {
+            //FileStream fileStream = new FileStream("", FileMode.CreateNew);
+
+        }
+    }
+}
